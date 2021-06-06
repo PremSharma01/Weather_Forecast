3b432511c2d4b7f69c0951c62ad07e6619c6d09f
Pushpender Singh <pushpendersingh694@gmail.com>
2021-06-06 15:49:58 +0530

    update ui and location

--- src/Fn/fn.js
+++ src/Fn/fn.js
@@ -1,34 +1,58 @@
 import { action, makeAutoObservable } from "mobx"
 import { create } from 'apisauce'
-import { APPID } from "../Api/config"
+import { APPID, location_key } from "../Api/config"
 const api = create({
     baseURL: 'https://api.openweathermap.org/data/2.5',
     headers: { Accept: 'application/vnd.github.v3+json' },
 })
+const location = create({
+    baseURL: 'https://us1.locationiq.com/v1',
+    headers: { Accept: 'application/vnd.github.v3+json' },
+})
 class Data {
-    error=false;
-    response=[];
-   loading=true
+    error = false;
+    response = [];
+    loading = true;
+    city = "";
 
     constructor() {
         makeAutoObservable(this)
         action(this.fatch)
     }
-    fatch=async(lat,lon)=>{
-      await  api
-        .get(`/forecast?lat=${lat}&lon=${lon}&APPID=${APPID}`)
-        .then(
-           async (response) =>{
-            if(response.ok){
-                this.response=await response.data.list
-                this.loading=false
+    fatch = async (lat, lon) => {
+        await api
+            .get(`/forecast?lat=${lat}&lon=${lon}&APPID=${APPID}`)
+            .then(
+                async (response) => {
+                    if (response.ok) {
+                        this.response = await response.data.list
+                        this.loading = false
 
-            }else{
-                this.loading=false
-                this.error=true
-            }
-                  
-            }
+                    } else {
+                        this.loading = false
+                        this.error = true
+                    }
+
+                }
+            )
+    }
+
+    fetchLocation = async (lat, lon) => {
+        await location
+            .get(`/reverse.php?key=${location_key}&lat=${lat}&lon=${lon}&format=json`)
+            .then(
+                async (response) => {
+                    if (response.ok) {
+                        this.city = await response.data.address.city
+                        this.loading = false
+
+                    } else {
+                        this.loading = false
+                        this.error = true
+                        console.log(response.data);
+                    }
+
+                }
             )
     }
 }
--- src/Screen/Error.js
+++ src/Screen/Error.js
@@ -1,26 +1,39 @@
 import { inject } from "mobx-react";
 import React, { Component } from "react";
-import { 
+import {
     View,
     Text,
     StyleSheet,
     TouchableOpacity
 } from "react-native";
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 @inject("store")
 class Error extends Component {
-    reload=async()=>{
-      this.props.store.error=false;
-      this.props.store.loading=true
+    reload = async () => {
+        this.props.store.error = false;
+        this.props.store.loading = true
         this.props.reload()
 
     }
     render() {
         return (
             <View style={styles.container}>
-                <Text>Error</Text>
-                <TouchableOpacity style={{backgroundColor:"blue"}} onPress={()=>this.reload()}>
-                    <Text style={{padding:10}}>Try again</Text>
-                </TouchableOpacity>
+                <View style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    paddingHorizontal: hp('4%')
+                }}>
+                    <Text style={{ fontSize: hp('7%') }}>Something</Text>
+                    <Text style={{ fontSize: hp('7%') }}>Went Wrong</Text>
+                    <Text style={{ fontSize: hp('7%') }}>at our End</Text>
+                    <View style={{ marginVertical: hp('5%') }} /> 
+                    <TouchableOpacity style={{ alignSelf: "center", borderColor: "#222", borderWidth: hp('0.2%') }} onPress={() => this.reload()}>
+                        <Text style={{ paddingVertical: hp('1.5%'), paddingHorizontal: hp('4%') }}>RETRY</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -30,7 +43,5 @@ export default Error;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
     }
 });
